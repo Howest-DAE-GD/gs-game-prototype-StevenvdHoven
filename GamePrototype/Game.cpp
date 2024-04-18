6db760a2d8b2114bf9f3716c66e80c2c0f531bf0
steven vd hoven <46068940+StevenvdHoven@users.noreply.github.com>
2024-04-18 15:51:40 +0200

    Changes

--- GamePrototype/Game.cpp
+++ GamePrototype/Game.cpp
@@ -1,27 +1,35 @@
 #include "pch.h"
 #include "Game.h"
 
-Game::Game( const Window& window ) 
-	:BaseGame{ window }
+Game::Game(const Window& window)
+	:BaseGame{ window },
+
+	m_MaxPlayerDistance{ 500 },
+	m_PlayerSpeed{ 200 },
+	m_Player1Position{ Point2f(window.width * 0.5f,window.height * 0.5f) - Vector2f(100,0) },
+	m_Player2Position{ Point2f(window.width * 0.5f,window.height * 0.5f) + Vector2f(100,0) },
+	m_Player1Direction{},
+	m_Player2Direction{},
+	m_CurrentBulletSpawn{ 20 }
 {
 	Initialize();
 }
 
-Game::~Game( )
+Game::~Game()
 {
-	Cleanup( );
+	Cleanup();
 }
 
-void Game::Initialize( )
+void Game::Initialize()
 {
-	
+	CreateBullets(10);
 }
 
-void Game::Cleanup( )
+void Game::Cleanup()
 {
 }
 
-void Game::Update( float elapsedSec )
+void Game::Update(float elapsedSec)
 {
 	// Check keyboard state
 	//const Uint8 *pStates = SDL_GetKeyboardState( nullptr );
@@ -33,42 +41,53 @@ void Game::Update( float elapsedSec )
 	//{
 	//	std::cout << "Left and up arrow keys are down\n";
 	//}
+	CheckInput();
+
+	SimulatePlayerMovement(elapsedSec);
+	CheckMaxDistance();
+
+	UpdateBullets(elapsedSec);
+	CheckBulletCollision();
+	CheckForRespawn();
 }
 
-void Game::Draw( ) const
+void Game::Draw() const
 {
-	ClearBackground( );
+	ClearBackground();
+
+	DrawPlayers();
+	DrawBullets();
+
 }
 
-void Game::ProcessKeyDownEvent( const SDL_KeyboardEvent & e )
+void Game::ProcessKeyDownEvent(const SDL_KeyboardEvent& e)
 {
 	//std::cout << "KEYDOWN event: " << e.keysym.sym << std::endl;
+	for (int index{ 0 }; index < m_CurrentInput.size(); ++index) {
+		if (m_CurrentInput[index] == e.keysym.sym) {
+			return;
+		}
+	}
+
+	m_CurrentInput.push_back(e.keysym.sym);
 }
 
-void Game::ProcessKeyUpEvent( const SDL_KeyboardEvent& e )
+void Game::ProcessKeyUpEvent(const SDL_KeyboardEvent& e)
 {
-	//std::cout << "KEYUP event: " << e.keysym.sym << std::endl;
-	//switch ( e.keysym.sym )
-	//{
-	//case SDLK_LEFT:
-	//	//std::cout << "Left arrow key released\n";
-	//	break;
-	//case SDLK_RIGHT:
-	//	//std::cout << "`Right arrow key released\n";
-	//	break;
-	//case SDLK_1:
-	//case SDLK_KP_1:
-	//	//std::cout << "Key 1 released\n";
-	//	break;
-	//}
+	for (int index{ 0 }; m_CurrentInput.size(); ++index) {
+		if (m_CurrentInput[index] == e.keysym.sym) {
+			m_CurrentInput.erase(std::next(m_CurrentInput.begin(), index));
+			return;
+		}
+	}
 }
 
-void Game::ProcessMouseMotionEvent( const SDL_MouseMotionEvent& e )
+void Game::ProcessMouseMotionEvent(const SDL_MouseMotionEvent& e)
 {
 	//std::cout << "MOUSEMOTION event: " << e.x << ", " << e.y << std::endl;
 }
 
-void Game::ProcessMouseDownEvent( const SDL_MouseButtonEvent& e )
+void Game::ProcessMouseDownEvent(const SDL_MouseButtonEvent& e)
 {
 	//std::cout << "MOUSEBUTTONDOWN event: ";
 	//switch ( e.button )
@@ -83,10 +102,10 @@ void Game::ProcessMouseDownEvent( const SDL_MouseButtonEvent& e )
 	//	std::cout << " middle button " << std::endl;
 	//	break;
 	//}
-	
+
 }
 
-void Game::ProcessMouseUpEvent( const SDL_MouseButtonEvent& e )
+void Game::ProcessMouseUpEvent(const SDL_MouseButtonEvent& e)
 {
 	//std::cout << "MOUSEBUTTONUP event: ";
 	//switch ( e.button )
@@ -103,8 +122,207 @@ void Game::ProcessMouseUpEvent( const SDL_MouseButtonEvent& e )
 	//}
 }
 
-void Game::ClearBackground( ) const
+void Game::ClearBackground() const
 {
-	glClearColor( 0.0f, 0.0f, 0.3f, 1.0f );
-	glClear( GL_COLOR_BUFFER_BIT );
+	glClearColor(0.0f, 0.0f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Game::CreateBullets(int amount)
+{
+	Rectf viewport{ GetViewPort() };
+	const Point2f center{ viewport.width * 0.5f,viewport.height * 0.5f };
+	for (int index{ 0 }; index < amount; ++index)
+	{
+
+		Point2f position
+		{
+			cosf((rand() % 361)) * (viewport.width * 0.75f) + viewport.width,
+			sinf((rand() % 361)) * (viewport.width * 0.75f) + viewport.height
+		};
+
+		Vector2f direction{ Vector2f(center) - Vector2f(position) };
+		direction = direction / direction.Length();
+		Bullet newBullet
+		{
+			position,
+			direction,
+			false
+		};
+
+		if (index >= m_Bullets.size()) {
+			m_Bullets.push_back(newBullet);
+		}
+		else {
+			m_Bullets[index] = newBullet;
+		}
+	}
+}
+
+void Game::UpdateBullets(float elpasedSec)
+{
+	Rectf viewport{ GetViewPort() };
+	for (int index{ 0 }; index < m_Bullets.size(); ++index)
+	{
+		if (m_Bullets[index].m_IsDead) {
+			continue;
+		}
+
+		Bullet& currentBullet{ m_Bullets[index] };
+		currentBullet.m_Position = currentBullet.m_Position + currentBullet.m_Direction * bulletSpeed * elpasedSec;
+		
+		const Circlef bullet{ currentBullet.m_Position,bulletRadius };
+		if (!utils::IsOverlapping(viewport,bullet)) {
+			currentBullet.m_IsDead = true;
+		}
+		
+	}
+}
+
+void Game::CheckBulletCollision()
+{
+	for (int index{ 0 }; index < m_Bullets.size(); ++index)
+	{
+		if (m_Bullets[index].m_IsDead) {
+			continue;
+		}
+
+		const Circlef player1{ m_Player1Position,playerRadius };
+		const Circlef player2{ m_Player2Position,playerRadius };
+		const Circlef bullet{ m_Bullets[index].m_Position,bulletRadius };
+
+		if (utils::IsOverlapping(player1, bullet) || utils::IsOverlapping(player2, bullet)) {
+			m_Bullets[index].m_IsDead = true;
+			HitRope();
+			return;
+		}
+	}
+}
+
+void Game::CheckForRespawn()
+{
+	for (int index{ 0 }; index < m_Bullets.size(); ++index) {
+		if (!m_Bullets[index].m_IsDead) {
+			return;
+		}
+	}
+
+	CreateBullets(m_CurrentBulletSpawn);
+	m_CurrentBulletSpawn += 10;
+
+}
+
+void Game::SimulatePlayerMovement(float elapsedSec)
+{
+	Rectf viewport{ GetViewPort() };
+	Rectf left{ -10,0,10,viewport.height };
+	Rectf bottom{ 0,-10,viewport.width,10 };
+	Rectf right{ viewport.width, 0, 10,viewport.height };
+	Rectf top{ 0,viewport.height,viewport.width,10 };
+
+	Point2f futurePlayer1Position = m_Player1Position + m_Player1Direction * m_PlayerSpeed * elapsedSec;
+	Point2f futurePlayer2Position = m_Player2Position + m_Player2Direction * m_PlayerSpeed * elapsedSec;
+
+
+	const Circlef player1{ futurePlayer1Position,playerRadius };
+	const Circlef player2{ futurePlayer2Position,playerRadius };
+
+	if (!utils::IsOverlapping(left, player1) && !utils::IsOverlapping(bottom, player1)
+		&& !utils::IsOverlapping(right, player1) && !utils::IsOverlapping(top, player1)) {
+		m_Player1Position = futurePlayer1Position;
+	}
+
+	if (!utils::IsOverlapping(left, player2) && !utils::IsOverlapping(bottom, player2)
+		&& !utils::IsOverlapping(right, player2) && !utils::IsOverlapping(top, player2)) {
+		m_Player2Position = futurePlayer2Position;
+	}
+
+
+}
+
+void Game::CheckMaxDistance()
+{
+
+	const float distance{ utils::GetDistance(m_Player1Position,m_Player2Position) };
+	if (distance > m_MaxPlayerDistance)
+	{
+		Vector2f player1Dir{ m_Player1Position - m_Player2Position };
+		player1Dir = player1Dir / player1Dir.Length();
+		Vector2f player2Dir{ -player1Dir };
+		const Point2f middle{ m_Player1Position + (m_Player2Position - m_Player1Position) * 0.5f };
+
+		m_Player1Position = middle + player1Dir * (m_MaxPlayerDistance * 0.5f * 0.9f);
+		m_Player2Position = middle + player2Dir * (m_MaxPlayerDistance * 0.5f * 0.9f);
+	}
+}
+
+void Game::HitRope()
+{
+	m_MaxPlayerDistance -= 100;
+	if (m_MaxPlayerDistance < 100) {
+		RestartGame();
+	}
+}
+
+void Game::CheckInput()
+{
+	m_Player1Direction = Vector2f{};
+	m_Player2Direction = Vector2f{};
+
+	for (int index{ 0 }; index < m_CurrentInput.size(); ++index) {
+		if (m_CurrentInput[index] == SDLK_w) m_Player1Direction.y += 1;
+		if (m_CurrentInput[index] == SDLK_s) m_Player1Direction.y += -1;
+		if (m_CurrentInput[index] == SDLK_d) m_Player1Direction.x += 1;
+		if (m_CurrentInput[index] == SDLK_a) m_Player1Direction.x += -1;
+
+		if (m_CurrentInput[index] == SDLK_UP) m_Player2Direction.y += 1;
+		if (m_CurrentInput[index] == SDLK_DOWN) m_Player2Direction.y += -1;
+		if (m_CurrentInput[index] == SDLK_RIGHT) m_Player2Direction.x += 1;
+		if (m_CurrentInput[index] == SDLK_LEFT) m_Player2Direction.x += -1;
+	}
+
+}
+
+void Game::RestartGame()
+{
+	Rectf window{ GetViewPort() };
+	m_MaxPlayerDistance = 500;
+	m_Player1Position = Point2f{ Point2f(window.width * 0.5f,window.height * 0.5f) - Vector2f(100,0) };
+	m_Player2Position = Point2f{ Point2f(window.width * 0.5f,window.height * 0.5f) + Vector2f(100,0) };
+	m_Player1Direction = Vector2f{};
+	m_Player2Direction = Vector2f{};
+	m_CurrentBulletSpawn = 20;
+
+	m_Bullets.clear();
+}
+
+void Game::DrawPlayers() const
+{
+	const float maxLineWidth{ 20.f };
+	const float distance{ utils::GetDistance(m_Player1Position,m_Player2Position) };
+	float percent{ (distance / m_MaxPlayerDistance) };
+	percent = 1.f - percent;
+
+	utils::SetColor(Color4f{ 0,.75f,0.2f,1 });
+	utils::DrawLine(m_Player1Position, m_Player2Position, 1.f + (maxLineWidth * percent));
+
+	utils::SetColor(player1Color);
+	utils::FillEllipse(m_Player1Position, playerRadius, playerRadius);
+
+	utils::SetColor(player2Color);
+	utils::FillEllipse(m_Player2Position, playerRadius, playerRadius);
+
+}
+
+void Game::DrawBullets() const
+{
+	for (int index{ 0 }; index < m_Bullets.size(); ++index)
+	{
+		if (!m_Bullets[index].m_IsDead)
+		{
+			utils::SetColor(Color4f{ 1,0,0,1 });
+			utils::FillEllipse(m_Bullets[index].m_Position, bulletRadius, bulletRadius);
+
+		}
+	}
 }
--- GamePrototype/Game.h
+++ GamePrototype/Game.h
@@ -1,5 +1,22 @@
 #pragma once
 #include "BaseGame.h"
+#include "utils.h"
+#include <vector>
+
+#define player1Color Color4f{0.f,1.f,0.f,1.f}
+#define player2Color Color4f{0.f,0.f,0.75f,1.f}
+#define playerRadius 50
+
+#define bulletRadius 10.f
+#define bulletSpeed 360.f
+
+struct Bullet 
+{
+	Point2f m_Position;
+	Vector2f m_Direction;
+	bool m_IsDead;
+};
+
 class Game : public BaseGame
 {
 public:
@@ -27,4 +44,37 @@ private:
 	void Initialize();
 	void Cleanup( );
 	void ClearBackground( ) const;
+
+	// Bullet functions
+	void CreateBullets(int amount);
+	void UpdateBullets(float elpasedSec);
+	void CheckBulletCollision();
+	void CheckForRespawn();
+
+	void SimulatePlayerMovement(float elapsedSec);
+	void CheckMaxDistance();
+	void HitRope();
+	void CheckInput();
+
+	void RestartGame();
+
+	void DrawPlayers() const;
+	void DrawBullets() const;
+
+
+	// VARIABLES
+	float m_MaxPlayerDistance;
+	float m_PlayerSpeed;
+
+	Point2f m_Player1Position;
+	Point2f m_Player2Position;
+
+	Vector2f m_Player1Direction;
+	Vector2f m_Player2Direction;
+
+	std::vector<int> m_CurrentInput;
+	std::vector<Bullet> m_Bullets;
+
+	int m_CurrentBulletSpawn;
+
 };
